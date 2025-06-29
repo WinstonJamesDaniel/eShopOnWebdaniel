104e94a48d20fd6b2c1ea5ef1f5a431a5a4d4148
Winston James Daniel B <126962245+WinstonJamesDaniel@users.noreply.github.com>
2025-06-29 20:34:03 +0530

    Update run-tests.ps1

--- run-tests.ps1
+++ run-tests.ps1
@@ -8,7 +8,7 @@ param(
 $vstestExe = "C:\\Program Files\\Microsoft Visual Studio\\2022\\Community\\Common7\\IDE\\CommonExtensions\\Microsoft\\TestWindow\\vstest.console.exe"
 
 # Temp .trx file
-$trxFile = "TestResults.trx"
+$trxFile = "$($dll.BaseName).trx"
 
 # -------- Read test list from JSON --------
 if (-Not (Test-Path $TestCasesFile)) {
